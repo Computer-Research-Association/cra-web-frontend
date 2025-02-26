e89967518928584c7f4db170a23fc14b9263dac6
min06150315 <min06150315@gmail.com>
2025-02-27 01:34:36 +0900

    [KB] fix[#363]: 댓글 불러오기 수정

--- src/components/Comment/List/CommetList.tsx
+++ src/components/Comment/List/CommetList.tsx
@@ -1,9 +1,5 @@
-import { useQuery } from '@tanstack/react-query';
-import { Comment } from '~/models/Comment.ts';
-import { QUERY_KEY } from '~/api/queryKey.ts';
-import { getCommentsByBoardId } from '~/api/comment.ts';
 import CommentItem from '~/components/Comment/Item/CommentItem.tsx';
-import LoadingSpinner from '~/components/Common/LoadingSpinner';
+// import LoadingSpinner from '~/components/Common/LoadingSpinner';
 import { Board } from '~/models/Board';
 
 export default function CommentList({ board }: { board: Board }) {
@@ -11,39 +7,29 @@ export default function CommentList({ board }: { board: Board }) {
   //   queryKey: QUERY_KEY.comment.commentsById(id),
   //   queryFn: async () => getCommentsByBoardId(id),
   // });
-
-  const commentsQuery = board.commentCount;
-  console.log(commentsQuery);
+  const commentsQuery = board.resListCommentDtos;
   let content;
 
   // if (commentsQuery.isLoading) {
   //   content = <LoadingSpinner />;
-  // } else if (commentsQuery.isError) {
-  //   content = <div className="error">에러가 발생했습니다!</div>;
-  // } else if (commentsQuery.isSuccess) {
-  //   content = commentsQuery.data.map((comment) => (
-  //     <div key={comment.id}>
-  //       <CommentItem
-  //         key={comment.id}
-  //         comment={comment}
-  //         isRoot={true}
-  //         commentsQuery={
-  //           commentsQuery.data.find((c) => c.id === comment.id) || null
-  //         }
-  //       />
-  //       {comment.commentList.map((childComment) => {
-  //         return (
-  //           <CommentItem
-  //             key={childComment.id}
-  //             comment={childComment}
-  //             isRoot={false}
-  //             commentsQuery={childComment}
-  //           />
-  //         );
-  //       })}
-  //     </div>
-  //   ));
-  // }
+  if (!commentsQuery) {
+    content = <div className="error">에러가 발생했습니다!</div>;
+  } else {
+    content = commentsQuery.map((comment) => (
+      <div key={comment.id}>
+        <CommentItem key={comment.id} comment={comment} isRoot={true} />
+        {comment.commentList.map((childComment) => {
+          return (
+            <CommentItem
+              key={childComment.id}
+              comment={childComment}
+              isRoot={false}
+            />
+          );
+        })}
+      </div>
+    ));
+  }
 
   return <div>{content}</div>;
 }
