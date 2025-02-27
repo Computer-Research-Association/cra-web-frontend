3e6d0e3c58efb7476b0c0389bf71a1a264687f27
min06150315 <min06150315@gmail.com>
2025-02-27 17:43:29 +0900

    [KB] fix[#363]: 비품 리스트 페이지네이션 구현 (CSS X)

--- src/pages/Board/Item/ItemPage.tsx
+++ src/pages/Board/Item/ItemPage.tsx
@@ -1,12 +1,6 @@
-import ItemList from '~/components/Item/List/ItemList.tsx';
+import ItemContainer from '~/components/Item/ItemContainer.tsx';
 import { ITEMCATEGORY } from '~/constants/itemCategory.ts';
-import styles from './ItemPage.module.css';
 
 export default function ItemPage() {
-  return (
-    <div className={styles['project-section']}>
-      <p>비품 페이지</p>
-      <ItemList itemCategory={ITEMCATEGORY.ITEM} />
-    </div>
-  );
+  return <ItemContainer category={ITEMCATEGORY.ITEM} />;
 }
