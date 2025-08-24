39eb59508fb57d52682dc11ccfa8a6185d97bcdc
Whal3 <leehan416@gmail.com>
2025-08-25 04:47:57 +0900

    [HG]hotfix: intro page 수정

--- src/components/Intro/Top/IntroTop.tsx
+++ src/components/Intro/Top/IntroTop.tsx
@@ -6,14 +6,21 @@ import Crang from '~/assets/images/Status_Crang.svg';
 import styles from './IntroTop.module.css';
 import recruitDate from '~/data/recruit-date.json';
 
+
+
+function toLocalDate(dateStr: string, endOfDay = false) {
+  const [y, m, d] = dateStr.split('-').map(Number);
+  return endOfDay
+    ? new Date(y, m - 1, d, 23, 59, 59, 999)
+    : new Date(y, m - 1, d, 0, 0, 0, 0);
+}
+
 const today = new Date();
 const month = today.getMonth() + 1;
-const RECRUIT_START_DATE = new Date(recruitDate.applicationPeriod.start);
-const RECRUIT_END_DATE = new Date(recruitDate.applicationPeriod.end);
-const isRecruitAvailable =
-  RECRUIT_END_DATE >= today && today >= RECRUIT_START_DATE;
+const RECRUIT_START_DATE = toLocalDate(recruitDate.applicationPeriod.start);
+const RECRUIT_END_DATE = toLocalDate(recruitDate.applicationPeriod.end, true);
 const RECRUIT_SEMESTER = recruitDate.semester ?? today.getFullYear() + '-' + (month > 7 ? 2 : 1);
-
+const isRecruitAvailable = today >= RECRUIT_START_DATE && today <= RECRUIT_END_DATE;
 function IntroTop({
   recruitRef,
   isHighlighted,
